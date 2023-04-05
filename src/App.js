d66260c816686202a25785efbd9d8c170781f735
kanuBang <gdrffg@gmail.com>
2023-04-05 12:05:16 +0900

    Props Part One

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
 
 
-// App 컴포넌트가 html을 반환한다.
-// App 함수가 반환한 HTML이 리액트 앱 화면에 그려진다.
+//props 이름: fav
+//fav에 "sushi"라는 값을 담아 Food 컴포넌트에 보냄
+// props에 있는 데이터는 문자열인 경우를 제외하면 모두 중괄호로 값을 감싼다.
+
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Potato />
+      <Food fav="sushi" somthis = {true} papapapa={['hello', 1, 2, 3, 4, true]}/>
     </div>
     
   ); 
 }
 
-// App 내부에 Potato 컴포넌트 만들기
-function Potato( ) {
+// props가 전달되는 과정
+// Food 컴포넌트에 전달한 props를 속성으로 가지는 객체가 출력됨
+function Food(props) {
+  console.log(props)
   return <h1>I LIKE POTATO</h1>
 }
+
 
 
 export default App;

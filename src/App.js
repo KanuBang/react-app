c02a60a60407c60071f6338164e14c87e00956ff
kanuBang <gdrffg@gmail.com>
2023-04-05 12:41:11 +0900

    Props Uning Map Part One

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
 import React from "react";
 
 
-
-// props가 전달되는 과정
-// Food 컴포넌트에 설정한 props가 전달됨
-// props를 구조 분해 할당으로 출력
-function Food({fav}) {
+function Food({name, pic}) {
   // {fav} = props; {fav}
   // props.fav
-  return <h1>I LIKE {fav}</h1>
+  return (
+    <div>
+      <h1>Best Player: {name}</h1>,
+      <img src={pic} ></img>
+    </div>
+  )
 }
+ 
+// map 함수는 배열의 모든 원소마다 특정 작업을 하는 함수를 적용하고
+// 그 함수가 반환한 결과를 모아서 배열로 반환한다.
+const players = [
+  {
+    name:"Theo Hernandez",
+    image: 'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt03ab16d69a741aa0/60db07560bc1b20fa6b6e295/9e3231059b9d40c4194744dab2b5936b1b35816f.jpg'
+  },
+  {
+    name:"Phil Foden",
+    image:'https://e0.365dm.com/21/04/2048x1152/skysports-phil-foden-manchester-city_5343992.jpg'
+  },
+  {
+    name:"Rodrigo",
+    image:"https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt497e050c56cd823b/61c82cc15d616f4311468f4b/Rodri_Man_City_UCL_2021-22.jpg"
+  },
+  {
+    name:"Rafael leao",
+    image:"https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt93fc37f9aeeab515/61ea94bf1ed54f3c3403cce6/leao-hands.jpg"
+  }
+];
 
-//props 이름: fav
-//fav에 "sushi"라는 값을 담아 Food 컴포넌트에 보냄
-// props에 있는 데이터는 문자열인 경우를 제외하면 모두 중괄호로 값을 감싼다.
+players.map((friend) => {
+  return friend + "$";
+})
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      {/*컴포넌트의 재사용*/}
-      <Food fav="sushi"/>
-      <Food fav="ramen"/>
-      <Food fav="icecream"/>
+      {players.map(player => (
+        <Food name = {player.name} pic = {player.image} />
+      ))}
     </div>
     
   ); 
 }
 
-/*
-1. JSX는 잗ㄴ지 HTML과 자바스크립트를 조합한 문법
-2. JSX를 이용해서 컴포넌트를 작성한다
-3. 컴포넌트의 이름은 대문자로 시작한다
-4. 컴포넌트에 데이터를 전달 할때는 props를 사용한다
-5. 컴포넌트에 props를 전달하면 props에 있는 데이터가 하나의
-객체로 변환된어 컴포넌트의 인자로 전달되고, 이걸 받아서 컴포넌트 함수에서 쓴다.
-6. ES6 구조 분헤 할당을 사용하면 props를 더 편리하게 사용 가능하다.
-*/
 export default App;

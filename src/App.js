4b3afd9de2489168002dbb560f43def2a6a12a5c
kanuBang <gdrffg@gmail.com>
2023-04-05 11:26:15 +0900

    Make Component Part 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Potato from "./Potato";
+
 
 // App 컴포넌트가 html을 반환한다.
 // App 함수가 반환한 HTML이 리액트 앱 화면에 그려진다.
@@ -10,11 +10,13 @@ function App() {
       <Potato />
     </div>
     
-    // Potato.js에 Potato 컴포넌트를 만든다 (JSX)
-    // App 컴포넌트의 반환 값으로 Potato 컴포넌트를 추가
-    // 리턴
-    // index.js 리턴 받은 값을 렌더링한다.
   ); 
 }
+
+// App 내부에 Potato 컴포넌트 만들기
+function Potato( ) {
+  return <h1>I LIKE POTATO</h1>
+}
+
 
 export default App;

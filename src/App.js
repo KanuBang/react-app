b6e5622a5c5f3c4a5b113f1aef57fab7eebdc2ac
kanuBang <gdrffg@gmail.com>
2023-04-05 11:19:08 +0900

    Make Component Part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Potato from "./Potato";
 
+// App 컴포넌트가 html을 반환한다.
+// App 함수가 반환한 HTML이 리액트 앱 화면에 그려진다.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <Potato />
     </div>
-  );
+    
+    // Potato.js에 Potato 컴포넌트를 만든다 (JSX)
+    // App 컴포넌트의 반환 값으로 Potato 컴포넌트를 추가
+    // 리턴
+    // index.js 리턴 받은 값을 렌더링한다.
+  ); 
 }
 
 export default App;
---
+++ src/Potato.js
@@ -0,0 +1,9 @@
+import React from "react";
+// 리액트가 JSX를 이해하기 위해 필요함
+
+// 컴포넌트의 기본 틀 + 컴포넌트 이름은 대문자로 시작해야(중요)
+function Potato( ) {
+    return <h3>love poatato</h3>;
+}
+
+export default Potato;

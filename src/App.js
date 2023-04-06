05c7a7faad0d5f0acc1138c1f00dbad1b80d9cd8
kanuBang <gdrffg@gmail.com>
2023-04-06 21:48:04 +0900

    Life Cycle

--- src/App.js
+++ src/App.js
@@ -5,7 +5,28 @@ import React from "react";
 // 필요한 부분만 바꿀 수 있기에 React는 화면 구성이 빠르다.
 // 리액트는 이전 state와 새로운 state를 비교하여 바뀐 데이터만 업데이트 한다.
 class App extends React.Component {
-  
+  // constructor()가 render() 함수보다 더 먼저 실행된다.
+  constructor(props) {
+    super(props)
+    console.log("Hello")
+  }
+
+  //render 함수가 실행된 다음 실행됨
+  //컴포넌트가 처음 화면에 그려지면 실행되는 함수
+  componentDidMount( ) {
+    console.log("component rendered")
+  }
+
+  //컴포넌트가 업데이트될 때 실행되는 함수
+  componentDidUpdate( ) {
+    console.log("component updated")
+  }
+
+  //컴포넌트가 화면에서 떠날 때 실행 됨.
+  componentWillUnmount( ) {
+    console.log("die")
+  }
+
   state = {
     count: 0,
   };
@@ -26,6 +47,7 @@ class App extends React.Component {
   }
 
   render( ) {
+    console.log("render")
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>

33a270d24082327cc70fb47db48eb09a07d063f0
kanuBang <gdrffg@gmail.com>
2023-04-06 16:21:57 +0900

    State Part Two

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from "react";
 
-// 마지막에 세미콜론 필수! 
-// state를 사용하려면 반드시 클래스형 컴포넌트 안에서, 소문자를 이용하여 state라고 적는다.
-// state는 동적 데이터를 저장할 수 있다.
-// 동적 데이터란 변경될 가능성이 있는 데이터를 말한다.
-
-//리액트는 state를 직접 변경하는 코드를 허용하지 않는다.
-//즉. add 함수 안에서 this.state.count = 1 해도 화면에는 변환가 없다.
- 
+// setState() 함수가 동작하면 state가 새로운 값으로 바뀐다.
+// 이어서 render 함수를 동작시켜 화면을 업데이트 한다.
+// 필요한 부분만 바꿀 수 있기에 React는 화면 구성이 빠르다.
+// 리액트는 이전 state와 새로운 state를 비교하여 바뀐 데이터만 업데이트 한다.
 class App extends React.Component {
+  
   state = {
     count: 0,
   };
-  add = ( ) => {
-    this.setState({count: 1})
-  }
 
+  add = ( ) => {
+    this.setState(current => ({
+      count: current.count + 1
+    }))
+  }
+  // state를 직접적으로 업데이트하기 보다는 콜백 함수를 전달하여 state를 업데이트 하자.
+  // state를 직접적으로 업데이트하면 성능 이슈가 발생
   minus = ( ) => {
     console.log("MINUS")
-    this.setState({count: -1})
+    // current에는 현재의 state가 넘어가고 그 state 객체의 count에 -1 한다.
+    this.setState(current => ({
+      count: current.count - 1
+    }))
   }
 
   render( ) {

350d87f8b5d1ed97c3ee7f6d25b6f8735f1a325c
kanuBang <gdrffg@gmail.com>
2023-04-06 15:56:53 +0900

    State

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React from "react";
 
-// App 클래스는 React.Component 클래스를 상속 받음. (필수)
-// APP 클래스가 React.Component 클래스에 있는 기능을 물려 받음.
-// 클래스 컴포넌트는 JSX를 반환해야 함.
-// 클래스는 함수가 아니기에 JSX를 반환하기 위해 render 함수를 이용
-// 함수형 컴포넌트는 return문이 JSX를 반환한다.
+// 마지막에 세미콜론 필수! 
+// state를 사용하려면 반드시 클래스형 컴포넌트 안에서, 소문자를 이용하여 state라고 적는다.
+// state는 동적 데이터를 저장할 수 있다.
+// 동적 데이터란 변경될 가능성이 있는 데이터를 말한다.
 
+//리액트는 state를 직접 변경하는 코드를 허용하지 않는다.
+//즉. add 함수 안에서 this.state.count = 1 해도 화면에는 변환가 없다.
+ 
 class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = ( ) => {
+    this.setState({count: 1})
+  }
+
+  minus = ( ) => {
+    console.log("MINUS")
+    this.setState({count: -1})
+  }
+
   render( ) {
-    return <h1>I'm s class Component</h1>;
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>   
+    )
   }
 }
 export default App;

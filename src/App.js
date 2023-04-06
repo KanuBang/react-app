918622a80cde199f7d336562dd86269ff80ceba7
kanuBang <gdrffg@gmail.com>
2023-04-06 23:36:25 +0900

    FEAT: Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css"
 
 class App extends React.Component {
   state = {
@@ -26,36 +27,44 @@ class App extends React.Component {
 
   render( ) {
     const {isLoading, movies} = this.state;
-    return <div>{isLoading ? "Loading..." : movies.map((movie) => {
-      console.log(movie)
-      return (
-        <Movie
-          id={movie.id}
-          year={movie.year}
-          title={movie.title}
-          summary={movie.summary}
-          poster={movie.medium_cover_image}
-        />
-      )
-    })} </div>;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+              <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
+              <Movie 
+                key = {movie.id}
+                id = {movie.id}
+                year = {movie.year}
+                title = {movie.title}
+                summary = {movie.summary}
+                poster = {movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    )
   }
 
 /* Flow
-1. render 함수 실행
-2. isLoading = true -> "Loading..."
-3. render 끝
-4. componentDidMount 실행
-5. this.getMovies() 실행
-6. api 끌어와서 promise 객체 movies에 저장
-7. setState로 동적 데이터 업데이트
-8. 동적 데이터가 업데이트 되었기 때문에 rerender
-9. render 함수 실행
-10. isLoading = true -> movies.map 실행
-11. map 함수의 콜백 함수 실행
-12. Movie 컴포넌트에 props 전달
-13. Movie 컴포넌트에서 <div><h4>{title}</h4></div> 리턴
-14. 콜백함수가 13번 결과값을 리턴
-15. 그 결과값을 렌더링
+1. render 실행
+2. 로딩
+3. render 실행 종료 후 componentDidMount 실행
+4. this.getMovies() 실행
+5. api를 이용하여 movies에 영화 data 담기
+6. setState로 state 업데이트
+7. rerender
+8. isLoading이 flase 이므로 movies.map 실행
+9. movies 객체 배열을 순회하며 순차적으로 Movie 컴포넌트에 props 값 전달
+10. Movie가 값을 리턴
+11. movies의 마지막 까지 콜백 함수 실행
+12. 최종 결과 렌더링
+
 */
 }
 

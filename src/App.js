216f7d1c4ed849461cf76027570a0592235293e5
kanuBang <gdrffg@gmail.com>
2023-04-06 15:42:32 +0900

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,14 @@
 import React from "react";
-import PropTypes from "prop-types"
 
+// App 클래스는 React.Component 클래스를 상속 받음. (필수)
+// APP 클래스가 React.Component 클래스에 있는 기능을 물려 받음.
+// 클래스 컴포넌트는 JSX를 반환해야 함.
+// 클래스는 함수가 아니기에 JSX를 반환하기 위해 render 함수를 이용
+// 함수형 컴포넌트는 return문이 JSX를 반환한다.
 
-function Food({name, pic, rating}) {
-
-  return (
-
-    <div>
-      <h1>Best Player: {name}</h1>
-      <h4>rating: {rating}</h4>
-      
-      <img src={pic} alt={name}></img>
-    </div>
-  )
-}
- 
-
-const players = [
-  /*id 부여: 컴포넌트가 서로 다르다는 걸 알리기 위하여 */
-  {
-    id: 1,
-    name:"Theo Hernandez",
-    image: 'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt03ab16d69a741aa0/60db07560bc1b20fa6b6e295/9e3231059b9d40c4194744dab2b5936b1b35816f.jpg',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name:"Phil Foden",
-    image:'https://e0.365dm.com/21/04/2048x1152/skysports-phil-foden-manchester-city_5343992.jpg',
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name:"Rodrigo",
-    image:"https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt497e050c56cd823b/61c82cc15d616f4311468f4b/Rodri_Man_City_UCL_2021-22.jpg",
-    rating: 2.8
-  },
-  {
-    id: 4,
-    name:"Rafael leao",
-    image:"https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt93fc37f9aeeab515/61ea94bf1ed54f3c3403cce6/leao-hands.jpg",
-    rating: 3.9
+class App extends React.Component {
+  render( ) {
+    return <h1>I'm s class Component</h1>;
   }
-];
-
-
-
-function App() {
-  
-  return (
-    <div>
-      {players.map(player => (
-         <Food rating = {player.rating} key ={player.id} name = {player.name} pic = {player.image} />
-      ))}
-    </div>
-    
-  ); 
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  pic: PropTypes.string.isRequired,
-  rating: PropTypes.number
 }
 export default App;
